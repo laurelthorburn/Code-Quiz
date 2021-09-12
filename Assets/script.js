32dd6b68d109ce3d7df0dc0c8f39aaf7f4e500e8
Laurel Thorburn <laurelthorburn@gmail.com>
2021-09-12 13:33:49 -0600

    Creating display function once quiz is initiated

--- Assets/script.js
+++ Assets/script.js
@@ -2,21 +2,32 @@
 //Global variables
 var timerEl = document.getElementById('countdown'); //grabs timer from index, location on page for timer
 var quizEl = document.getElementById('quiz'); //grabs quz paragraph from index, location on page for quiz questions
-var welcomePage = document.getElementById('welcome-page');
-var quizQuestion = document.getElementById('quiz');
+
+var welcomePage = document.getElementById('welcome-page'); //Welcome Page
+
+var quizDiv = document.getElementById('quiz'); //Main Quiz Container
+
+// grabbing quiz content
+var quizQuestion = document.getElementById('quiz-question');
 var answer1 = document.getElementById('li1');
 var answer2 = document.getElementById('li2');
 var answer3 = document.getElementById('li3');
 var answer4 = document.getElementById('li4');
 
+// grabbing buttons
 var startBtn = document.getElementById('begin-btn');
 var submitBtn = document.getElementById('submit-btn');
+var btnA = document.getElementById('btn-a');
+var btnB = document.getElementById('btn-b');
+var btnC = document.getElementById('btn-c');
+var btnD = document.getElementById('btn-d');
 
 // Beginning main function that contains timer and quiz questions
 var beginQuiz = function countdown() {
     // Welcome Page disappears on click (click event below)
     function changeVisibility(){
         welcomePage.setAttribute("style", "display: none; ");
+        quizDiv.setAttribute("style", "visibility: visible;");
     };
 
 changeVisibility();
