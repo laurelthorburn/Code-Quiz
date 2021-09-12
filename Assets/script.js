3da2dc55567d43b06334b664d76b939460bb4e62
Laurel Thorburn <laurelthorburn@gmail.com>
2021-09-12 10:44:47 -0600

    Modified timer code

--- Assets/script.js
+++ Assets/script.js
@@ -1,31 +1,49 @@
-var timerEl = document.getElementById('countdown'); //grabs countdown from index
 
+//Global variables
+var timerEl = document.getElementById('countdown'); //grabs timer from index, location on page for timer
+var quizEl = document.getElementById('quiz'); //grabs quz paragraph from index, location on page for quiz questions
 
-
-// Creating timer and function that follows
+// Beginning main function that contains timer and quiz questions
 function countdown() {
-  var timeLeft = 30;
+  var timeLeft = 31; //quiz time in msec, multiplied by 1000 below to covert to seconds
 
-  var timeInterval = (function () {
-    
-    timerEl.textContent = timeLeft + " seconds remaining."; // shows this first so starts at 5
-    
+  // TO DO: Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
+  var timeInterval = setInterval(function () {
     timeLeft--; //then begins the countdown
-    
-    // Creating quiz sequence as long as there is time left
-    while (timeLeft != 0){
 
+    timerEl.textContent = timeLeft + " seconds remaining."; // shows this first so starts at 5
+
+    if (timeLeft === 1) { //WHY DO I SKIP 2
+      timerEl.textContent = timeLeft + " second remaining.";
     }
 
-    if(timeLeft === 0) { 
+    if(timeLeft === 0) { // was stopping at 2 when at 0
       // Stops execution of action at set interval
       clearInterval(timeInterval);
-      timerEl.textContent = "Time is up!  Please enter your initials below to store your score.";
-      // Calls function to display words
+      timerEl.textContent = "Time is up!";
+
+      displayMessage();
     }
 
   }, 1000);
-};
+}
 
- countdown();
+// Displays the message one word at a time
+// function displayMessage() {
+//   var wordCount = 0;
 
+//   // Uses the `setInterval()` method to call a function to be executed every 1000 milliseconds
+//   var msgInterval = setInterval(function () {
+//     // If there are no more words left in the message
+//     if (words[wordCount] === undefined) {
+//       // Use `clearInterval()` to stop the timer
+//       clearInterval(msgInterval);
+//     } else {
+//       // Display one word of the message
+//       mainEl.textContent = words[wordCount];
+//       wordCount++;
+//     }
+//   }, 1000);
+// }
+
+countdown();
