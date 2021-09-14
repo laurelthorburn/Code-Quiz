cfdfb1a320c85b9c9c18135cdd46a556016849b4
Laurel Thorburn <laurelthorburn@gmail.com>
2021-09-14 16:45:24 -0600

    Working on start quiz button funnction

--- Assets/script.js
+++ Assets/script.js
@@ -2,13 +2,21 @@
 const question = document.querySelector("#quiz-question");
 const choices = document.querySelectorAll(".choice-text");
 const scoreText = document.querySelector("#score");
+const progressText = document.querySelector("#");
+
+var timerEl = document.getElementById('countdown'); //grabs timer from index, location on page for timer
+var welcomePage = document.getElementById('welcome-page'); //Welcome Page
+var quizDiv = document.getElementById('quiz-container'); //Main Quiz Container
+
 
 let currentQuestion = {};
 let acceptingAnswers = true;
 
-let score =0;
+let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
+
+var startBtn = document.getElementById('begin-btn');
 
 //Quiz Questions Set in an Array
 var questions = [
@@ -62,11 +70,53 @@ var questions = [
 const SCORE_POINTS = 100;
 const MAX_QUESTIONS = 5;
 
+
+// Beginning main function that contains timer and quiz questions
+
+var beginQuiz = function countdown() {
+    // Welcome Page disappears on click (click event below)
+    function changeVisibility(){
+        welcomePage.setAttribute("style", "display: none; ");
+        quizDiv.setAttribute("style", "visibility: visible;");
+    };
+
+
+//Begin Timer
+  var timeLeft = 76; //quiz time in msec, multiplied by 1000 below to covert to seconds
+
+  var timeInterval = setInterval(function () {
+    timeLeft--; //Begins the countdown
+
+    timerEl.textContent = "Time: " + timeLeft + " seconds"; 
+
+    if (timeLeft === 1) { 
+      timerEl.textContent = "Time: " + timeLeft + " second";
+    }
+
+    if(timeLeft === 0) { 
+      // Stops execution of action at set interval
+      clearInterval(timeInterval);
+      timerEl.textContent = "Time is up!";
+
+    //   displayMessage(); -- eventually this will be high score page?
+    }
+
+  }, 1000);
+
+
 startGame = () => {
     questionCounter = 0;
     score = 0;
     availableQuestions = [...questions];
     getNewQuestion();
+
+// Welcome Page disappears on click (click event below)
+    function changeVisibility(){
+        welcomePage.setAttribute("style", "display: none; ");
+        quizDiv.setAttribute("style", "visibility: visible;");
+    };
+    
+    changeVisibility();
 }
 
 getNewQuestion = () => {
@@ -76,7 +126,7 @@ getNewQuestion = () => {
         return window.location.assign('/end.html');
     }
     questionCounter++
-    ProgressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`;
+    progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`;
     
     const questionsIndex = Math.floor(Math.random() * availableQuestions.length)
     currentQuestion = availableQuestions[questionsIndex];
@@ -103,8 +153,17 @@ choices.forEach(choice => {
         let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect'
 
         if(classToApply === 'correct'){
-            increment.score (SCORE_POINTS)
+            incrementScore (SCORE_POINTS)
         }
-
-    })
+        selectedChoice.parentElement.classList.remove(classToApply);
+        getNewQuestion();
+    }, 1000)
 })
+
+incrementScore = num => {
+    score += num;
+    scoreText.innerText = score;
+}
+}
+
+startBtn.addEventListener("click", beginQuiz);
