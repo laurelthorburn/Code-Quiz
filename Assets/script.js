e3e11ac511500dcaa598f0bbbb788ed50ab509be
Laurel Thorburn <laurelthorburn@gmail.com>
2021-09-16 12:21:44 -0600

    Added comments to array to help understanding of how to call each array object

--- Assets/script.js
+++ Assets/script.js
@@ -1,17 +1,15 @@
 //Global variables
 var timerEl = document.getElementById('countdown'); //grabs timer from index, location on page for timer
-var quizEl = document.getElementById('quiz'); //grabs quz paragraph from index, location on page for quiz questions
-
 var welcomePage = document.getElementById('welcome-page'); //Welcome Page
-
 var quizDiv = document.getElementById('quiz'); //Main Quiz Container
+var megaBtn = document.getElementsByTagName('ol');
 
 // grabbing quiz content
 var quizQuestion = document.getElementById('quiz-question');
-var answer1 = document.getElementById('li1');
-var answer2 = document.getElementById('li2');
-var answer3 = document.getElementById('li3');
-var answer4 = document.getElementById('li4');
+var choiceOne = document.getElementById('li1');
+var choiceTwo = document.getElementById('li2');
+var choiceThree = document.getElementById('li3');
+var choiceFour = document.getElementById('li4');
 
 // grabbing buttons
 var startBtn = document.getElementById('begin-btn');
@@ -21,6 +19,46 @@ var btnB = document.getElementById('btn-b');
 var btnC = document.getElementById('btn-c');
 var btnD = document.getElementById('btn-d');
 
+// empty array to be added/removed from later
+// var quizContent = [];
+ var index = 0; //referencing starting of array at 0 to add onto later
+
+var quizContent = [
+  {
+  question: "Sample Question 1", //quizContent[0].question 
+  choice1: "aSample Answer 1", //quizContent[0].choice1 
+  choice2: "Sample Answer 2", //quizContent[0].choice2
+  choice3: "Sample Answer 3",  //quizContent[0].choice3 
+  choice4: "Sample Answer 4",  //quizContent[0].choice4 
+  answer: "a",  //  //quizContent[0].answer
+},
+{
+  question: "Sample Question 2", //quizContent[1].question 
+  choice1: "bSample Answer 1",  //quizContent[1].choice1
+  choice2: "Sample Answer 2",  //quizContent[1].choice2
+  choice3: "Sample Answer 3",   //quizContent[1].choice3
+  choice4: "Sample Answer 4",  //quizContent[1].choice4
+  answer: "b",
+},
+{
+  question: "Sample Question 3",
+  choice1: "cSample Answer 1",
+  choice2: "Sample Answer 2",
+  choice3: "Sample Answer 3", 
+  choice4: "Sample Answer 4",
+  answer: "a",
+},
+{
+  question: "Sample Question 4",
+  choice1: "dSample Answer 1",
+  choice2: "Sample Answer 2",
+  choice3: "Sample Answer 3", 
+  choice4: "Sample Answer 4",
+  answer: "c",
+}
+];
+
+// ============================== THE MEGA FUNCTION ==============================
 // Beginning main function that contains timer and quiz questions
 var beginQuiz = function countdown() {
     // Welcome Page disappears on click (click event below)
@@ -29,7 +67,7 @@ var beginQuiz = function countdown() {
         quizDiv.setAttribute("style", "visibility: visible;");
     };
 
-changeVisibility();
+changeVisibility(); // hiding welcome page and shifting to questions
 
 //Begin Timer
   var timeLeft = 76; //quiz time in msec, multiplied by 1000 below to covert to seconds
@@ -53,59 +91,23 @@ changeVisibility();
 
   }, 1000);
 
-//Begin Quiz Questions
-function mainQuiz(){
+  function mainQuiz(){
+   // can i create an array with question, choices, and answer.  Pull from array using the for loop that adds one to the index each time so questions shift through but also have if user clicks answer that is equal to answer then does X if not does Y.
+    var currentQuestion = quizContent[index]; // accessing same if [0]
+    index++;
 
-    function questionOne(){
-        quizQuestion.textContent = "Sample Question 1"; 
-        answer1.textContent = "aSample Answer 1"; 
-        answer2.textContent = "Sample Answer 2"; 
-        answer3.textContent = "Sample Answer 3"; 
-        answer4.textContent = "Sample Answer 4"; 
-    };
-    questionOne();
+    //create onclick event so if correct = x else = y
+  }
 
-    function questionTwo(){
-        quizQuestion.textContent = "Sample Question 2"; 
-        answer1.textContent = "bSample Answer 1"; 
-        answer2.textContent = "Sample Answer 2"; 
-        answer3.textContent = "Sample Answer 3"; 
-        answer4.textContent = "Sample Answer 4"; 
-    };
-    questionTwo();
-
-    function questionThree(){
-        quizQuestion.textContent = "Sample Question 3"; 
-        answer1.textContent = "cSample Answer 1"; 
-        answer2.textContent = "Sample Answer 2"; 
-        answer3.textContent = "Sample Answer 3"; 
-        answer4.textContent = "Sample Answer 4"; 
-    };
-    questionThree();
-
-    function questionFour(){
-        quizQuestion.textContent = "Sample Question 4"; 
-        answer1.textContent = "dSample Answer 1"; 
-        answer2.textContent = "Sample Answer 2"; 
-        answer3.textContent = "Sample Answer 3"; 
-        answer4.textContent = "Sample Answer 4"; 
-     };
-     questionFour();
-
-    function questionFive(){
-        quizQuestion.textContent = "Sample Question 5"; 
-        answer1.textContent = "eSample Answer 1"; 
-        answer2.textContent = "Sample Answer 2"; 
-        answer3.textContent = "Sample Answer 3"; 
-        answer4.textContent = "Sample Answer 4"; 
-    };
-    questionFive();
+  mainQuiz();
 }
 
-mainQuiz();
-
-};
+// ============================== THE END OF MEGA FUNCTION ==============================
 
 
 //Quiz/timer starts when start quiz button is clicked
 startBtn.addEventListener("click", beginQuiz);
+
+btnA.addEventListener("click", function(){
+  
+})
