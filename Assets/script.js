90d5b047fc092530a372d3f25376e6152d7f4ec5
Laurel Thorburn <laurelthorburn@gmail.com>
2021-09-16 17:47:30 -0600

    Revising code, Thank you Robert!

--- Assets/script.js
+++ Assets/script.js
@@ -6,10 +6,10 @@ var megaBtn = document.getElementsByTagName('ol');
 
 // grabbing quiz content
 var quizQuestion = document.getElementById('quiz-question');
-var choiceOne = document.getElementById('li1');
-var choiceTwo = document.getElementById('li2');
-var choiceThree = document.getElementById('li3');
-var choiceFour = document.getElementById('li4');
+var choiceOneLi = document.getElementById('li1');
+var choiceTwoLi = document.getElementById('li2');
+var choiceThreeLi = document.getElementById('li3');
+var choiceFourLi = document.getElementById('li4');
 
 // grabbing buttons
 var startBtn = document.getElementById('begin-btn');
@@ -19,44 +19,49 @@ var btnB = document.getElementById('btn-b');
 var btnC = document.getElementById('btn-c');
 var btnD = document.getElementById('btn-d');
 
-// empty array to be added/removed from later
-// var quizContent = [];
- var index = 0; //referencing starting of array at 0 to add onto later
-
 var quizContent = [
   {
   question: "Sample Question 1", //quizContent[0].question 
-  choice1: "aSample Answer 1", //quizContent[0].choice1 
-  choice2: "Sample Answer 2", //quizContent[0].choice2
-  choice3: "Sample Answer 3",  //quizContent[0].choice3 
-  choice4: "Sample Answer 4",  //quizContent[0].choice4 
-  answer: "a",  //  //quizContent[0].answer
+  choices: ["aSample Answer 1", "aSample Answer 2","aSample Answer 3", "aSample Answer 4"], //quizContent[0].choices[0-4444] 
+  answer: "aSample Answer 1",  //  //quizContent[0].answer
 },
 {
   question: "Sample Question 2", //quizContent[1].question 
-  choice1: "bSample Answer 1",  //quizContent[1].choice1
-  choice2: "Sample Answer 2",  //quizContent[1].choice2
-  choice3: "Sample Answer 3",   //quizContent[1].choice3
-  choice4: "Sample Answer 4",  //quizContent[1].choice4
-  answer: "b",
+  choices: ["aSample Answer 1", "aSample Answer 2","aSample Answer 3", "aSample Answer 4"],
+  answer: "aSample Answer 3",
 },
 {
   question: "Sample Question 3",
-  choice1: "cSample Answer 1",
-  choice2: "Sample Answer 2",
-  choice3: "Sample Answer 3", 
-  choice4: "Sample Answer 4",
-  answer: "a",
+  choices: ["aSample Answer 1", "aSample Answer 2","aSample Answer 3", "aSample Answer 4"],
+  answer: "aSample Answer 3",
 },
 {
   question: "Sample Question 4",
-  choice1: "dSample Answer 1",
-  choice2: "Sample Answer 2",
-  choice3: "Sample Answer 3", 
-  choice4: "Sample Answer 4",
-  answer: "c",
+  choices: ["aSample Answer 1", "aSample Answer 2","aSample Answer 3", "aSample Answer 4"],
+  answer: "aSample Answer 2",
 }
 ];
+
+// empty array to be added/removed from later
+// var quizContent = [];
+var index = 0; //referencing starting of array at 0 to add onto later
+var currentQuiz = quizContent[index]; // accessing same if [0]
+
+// Global functions - incrementor
+function nextQuestion(){
+  index ++;
+  userClick();
+}
+
+function userClick(){
+  if(choiceOneLi.innerText === quizContent[index].answer){
+    console.log("YAY!!!");
+  } else {
+    console.log("Ugh...javascript...am i right?");
+  }
+}
+
+
 
 // ============================== THE MEGA FUNCTION ==============================
 // Beginning main function that contains timer and quiz questions
@@ -91,15 +96,7 @@ changeVisibility(); // hiding welcome page and shifting to questions
 
   }, 1000);
 
-  function mainQuiz(){
-   // can i create an array with question, choices, and answer.  Pull from array using the for loop that adds one to the index each time so questions shift through but also have if user clicks answer that is equal to answer then does X if not does Y.
-    var currentQuestion = quizContent[index]; // accessing same if [0]
-    index++;
 
-    //create onclick event so if correct = x else = y
-  }
-
-  mainQuiz();
 }
 
 // ============================== THE END OF MEGA FUNCTION ==============================
@@ -108,6 +105,6 @@ changeVisibility(); // hiding welcome page and shifting to questions
 //Quiz/timer starts when start quiz button is clicked
 startBtn.addEventListener("click", beginQuiz);
 
-btnA.addEventListener("click", function(){
-  
-})
+// btnA.addEventListener("click", function(){
+//   mainQuiz();
+// })
